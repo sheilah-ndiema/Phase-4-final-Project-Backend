3f6e62e77655b10a0fdd8cc832e28d921370e3e5
Ndiema-S <you@example.com>
2022-12-06 12:23:37 +0300

    Add sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+          render json: user
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
+    end
