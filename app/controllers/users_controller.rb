b8ae9b56e1d2b48184a84ce3ee28ca5ddb6b19eb
Ndiema-S <you@example.com>
2022-12-07 21:19:31 +0300

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
-    #add
-    skip_before_action only: [:show]
-    
+    wrap_parameters false
+
+    def create
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+    end
+
     def show
         user = User.find_by(id: session[:user_id])
         if user
-          render json: user
+          render json: user, status: :ok
         else
           render json: { error: "Not authorized" }, status: :unauthorized
         end
-      end
     end
+
+      private
+      def user_params
+          params.permit(:username, :password, :password_confirmation)
+      end
+end
 
