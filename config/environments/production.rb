48b090cfb13c2ac5422155742d089479e7b84fcc
Ndiema-S <you@example.com>
2022-12-08 00:10:56 +0300

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.hosts << "bookitapi-production.up.railway.app"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
