089afe2998900e7747893622b7e12db25bc178b3
Ndiema-S <you@example.com>
2022-12-07 21:20:27 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   #resources :users
   resources :books, only: [:index, :show, :create, :destroy]
-  post "/login", to: "users#show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
